9a2cba634f6225a8d8580b2fcbdb846c9b9cc52e
AugustoCVS <guty.vertolis@gmail.com>
2024-06-04 17:38:01 -0300

    feat: add filter section

--- src/screens/Home/home.screen.tsx
+++ src/screens/Home/home.screen.tsx
@@ -1,11 +1,19 @@
+import { FilterSection } from "./components/FilterSection/filter-section.component";
 import { ProductCard } from "./components/ProductCard/product-card.component";
 import { useHome } from "./home.hook";
 
 export const Home: React.FC = () => {
-  const { states } = useHome();
+  const { states, actions } = useHome();
 
   return (
     <div className="w-full h-full min-h-screen flex flex-col items-center bg-gray-800">
+      <FilterSection
+        filter={states.filter}
+        setFilter={actions.setFilter}
+        search={states.search}
+        setSearch={actions.setSearch}
+      />
+
       <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {states.products?.map((product) => (
           <ProductCard key={product.id} product={product} />

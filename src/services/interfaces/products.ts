28cca3991c29df9401fedc8a7ef0f4275a959ebc
AugustoCVS <guty.vertolis@gmail.com>
2024-06-04 13:58:12 -0300

    feat: create products service

---
+++ src/services/interfaces/products.ts
@@ -0,0 +1,78 @@
+export enum CategoryType {
+  "beauty",
+  "fragrances",
+  "furniture",
+  "groceries",
+  "home-decoration",
+  "kitchen-accessories",
+  "laptops",
+  "mens-shirts",
+  "mens-shoes",
+  "mens-watches",
+  "mobile-accessories",
+  "motorcycle",
+  "skin-care",
+  "smartphones",
+  "sports-accessories",
+  "sunglasses",
+  "tablets",
+  "tops",
+  "vehicle",
+  "womens-bags",
+  "womens-dresses",
+  "womens-jewellery",
+  "womens-shoes",
+  "womens-watches",
+}
+
+export type ReviewProps = {
+  rating: number;
+  comment: string;
+  date: Date;
+  reviewerName: string;
+  reviewerEmail: string;
+};
+
+export type ProductProps = {
+  id: number;
+  title: string;
+  description: string;
+  category: CategoryType;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: CategoryType[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: {
+    width: number;
+    height: number;
+    depth: number;
+  };
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: ReviewProps[];
+  returnPolicy: string;
+  minimumOrderQuantity: number;
+  meta: {
+    createdAt: Date;
+    updatedAt: Date;
+    barcode: string;
+    qrCode: string;
+  };
+  images: string[];
+  thumbnail: string;
+};
+
+
+export type ProductsResponse = {
+  products: ProductProps[];
+}
+
+export type ProductRequestProps = {
+  sortBy?: string;
+  order?: string;
+}
---
+++ src/services/products.ts
@@ -0,0 +1,16 @@
+import { api } from "./api";
+import { CategoryType, ProductRequestProps, ProductsResponse } from "./interfaces/products";
+
+export const ProductsService = {
+  getAllProducts: async ({order, sortBy}: ProductRequestProps): Promise<ProductsResponse> => {
+    const res = await api.get<ProductsResponse>(`/products?sortBy=${sortBy}&order=${order}`);
+
+    return res.data;
+  },
+
+  filterByCategory: async ({category}: {category: CategoryType}): Promise<ProductsResponse> => {
+    const res = await api.get<ProductsResponse>(`/products/category${category}`);
+
+    return res.data;
+  }
+};

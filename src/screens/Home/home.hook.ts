567d894f949817e2a923d6347d459f66aa02aa00
AugustoCVS <guty.vertolis@gmail.com>
2024-06-04 17:37:33 -0300

    feat: add filter

--- src/screens/Home/home.hook.ts
+++ src/screens/Home/home.hook.ts
@@ -1,29 +1,58 @@
 import { ProductsService } from "@/services/products";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { OrderOptions, filterProps } from "./home.types";
 
 export const useHome = () => {
-  const [sortBy, setSortBy] = useState<string>("");
-  const [order, setOrder] = useState<"asc" | "desc">("desc");
   const [search, setSearch] = useState<string>("");
+  const [filter, setFilter] = useState<filterProps>({
+    sortBy: "",
+    order: OrderOptions.DESC,
+    category: "",
+  });
 
   const { data, isLoading } = useQuery({
-    queryKey: ["products"],
+    queryKey: ["products", filter],
     queryFn: async () =>
       await ProductsService.getAllProducts({
-        sortBy: sortBy,
-        order: order,
+        sortBy: filter.sortBy,
+        order: filter.order,
         search: search,
       }),
   });
+
+  const { data: test } = useQuery({
+    queryKey: ["categories"],
+    queryFn: async () =>
+      await ProductsService.filterByCategory({
+        category: filter.category,
+      }),
+    enabled: filter.category !== "",
+  });
+
+  const handleClearFilter = (): void => {
+    setFilter({
+      sortBy: "",
+      order: OrderOptions.DESC,
+      category: "",
+    });
+  };
+
+  console.log("PRODUTOS FILTRADOS", test?.products)
 
   const products = data?.products;
 
   return {
     states: {
+      filter,
       products,
       isLoading,
       search,
+    },
+    actions: {
+      setSearch,
+      setFilter,
+      handleClearFilter,
     },
   };
 };

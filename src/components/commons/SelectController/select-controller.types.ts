6afd7b94946dfc060a3e2e158cdc203def6ab186
AugustoCVS <guty.vertolis@gmail.com>
2024-06-04 15:03:52 -0300

    chore: remove unused code

--- src/components/commons/SelectController/select-controller.types.ts
+++
@@ -1,9 +0,0 @@
-export type SelectControllerProps = {
-  control: any;
-  name: string;
-  errorMessage: string | undefined;
-  isInvalid: boolean;
-  children: React.ReactNode;
-  defaultValue?: string;
-  label: string;
-};

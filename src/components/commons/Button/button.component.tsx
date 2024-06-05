4633c3a055068875c0f7f992888d7aea8e969fc9
AugustoCVS <guty.vertolis@gmail.com>
2024-06-04 22:37:43 -0300

    refactor: adjust button width

--- src/components/commons/Button/button.component.tsx
+++ src/components/commons/Button/button.component.tsx
@@ -12,7 +12,7 @@ export const Button: React.FC<ButtonProps> = ({
       className={
         className
           ? className
-          : "w-full items-center justify-center p-4 border border-gray-500 bg-gray-700 text-gray-100 font-regular hover:bg-gray-500 hover:text-gray-700 rounded-lg disabled:bg-gray-500 disabled:hover:text-gray-100 disabled:cursor-not-allowed"
+          : "w-80 items-center justify-center p-2.5 border border-gray-500 bg-gray-700 text-gray-100 font-regular hover:bg-gray-500 hover:text-gray-700 rounded-lg disabled:bg-gray-500 disabled:hover:text-gray-100 disabled:cursor-not-allowed"
       }
       onClick={onClick}
       disabled={loading}

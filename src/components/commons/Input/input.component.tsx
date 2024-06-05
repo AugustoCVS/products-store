27c8094d42d205c7c5d077437e79ad74ee5378df
AugustoCVS <guty.vertolis@gmail.com>
2024-06-05 04:02:08 -0300

    feat: add search icon

--- src/components/commons/Input/input.component.tsx
+++ src/components/commons/Input/input.component.tsx
@@ -1,3 +1,4 @@
+import { Search } from "lucide-react";
 import { InputProps } from "./input.types";
 
 export const Input: React.FC<InputProps> = ({
@@ -8,15 +9,19 @@ export const Input: React.FC<InputProps> = ({
   disabled,
 }) => {
   return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      disabled={disabled}
-      className={`text-gray-300 bg-gray-900 p-4 flex items-center w-full text-sm rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500
+    <div className="relative">
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+        className={`text-gray-300 bg-gray-900 p-4 flex items-center w-full text-sm rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500
       disabled:cursor-not-allowed
         `}
-    />
+      />
+
+      <Search className="absolute right-3.5 top-3.5 text-gray-300" />
+    </div>
   );
 };

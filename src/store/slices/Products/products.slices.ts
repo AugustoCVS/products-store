e1c36cd68307e06a749cff975c68cdd2d1b89d81
AugustoCVS <guty.vertolis@gmail.com>
2024-06-04 23:18:50 -0300

    feat: add product

--- src/store/slices/Products/products.slices.ts
+++ src/store/slices/Products/products.slices.ts
@@ -1,18 +1,58 @@
-import { ProductProps } from "@/services/interfaces/products";
+import { CategoryType, ProductProps } from "@/services/interfaces/products";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-const initialState: ProductProps[] = [];	
+const initialProductsListState: ProductProps[] = [];
+const initialProductState: ProductProps = {
+  id: 0,
+  title: "",
+  description: "",
+  category: "" as unknown as CategoryType,
+  price: 0,
+  discountPercentage: 0,
+  rating: 0,
+  stock: 0,
+  tags: [],
+  brand: "",
+  sku: "",
+  weight: 0,
+  dimensions: {
+    width: 0,
+    height: 0,
+    depth: 0,
+  },
+  warrantyInformation: "",
+  shippingInformation: "",
+  availabilityStatus: "",
+  reviews: [],
+  returnPolicy: "",
+  minimumOrderQuantity: 0,
+  meta: {
+    createdAt: new Date,
+    updatedAt: new Date,
+    barcode: "",
+    qrCode: "",
+  },
+  images: [],
+  thumbnail: "",
+};
 
 const productSlice = createSlice({
-  name: 'products',
-  initialState,
+  name: "products",
+  initialState: {
+    productsList: initialProductsListState,
+    product: initialProductState,
+  },
   reducers: {
-    setProducts: (_, action: PayloadAction<ProductProps[]>) => {
-      return action.payload;
-    }
-  }
-})
+    setProductsList: (state, action: PayloadAction<ProductProps[]>) => {
+      state.productsList = action.payload;
+    },
 
-export const { setProducts } = productSlice.actions;
+    setProduct: (state, action: PayloadAction<ProductProps>) => {
+      state.product = action.payload;
+    },
+  },
+});
+
+export const { setProductsList, setProduct } = productSlice.actions;
 
 export default productSlice.reducer;

7972e4dbd1e978c0fff2cce9221fac3fe7770390
AugustoCVS <guty.vertolis@gmail.com>
2024-06-05 00:29:50 -0300

    feat: add swiper to images

--- src/components/commons/ModalProductInfo/components/Image/image.component.tsx
+++ src/components/commons/ModalProductInfo/components/Image/image.component.tsx
@@ -1,3 +1,35 @@
-export const Image: React.FC<{ src: string; alt: string }> = ({ src, alt }) => {
-  return <img src={src} alt={alt} className="bg-black w-[320px] h-[360px]" />;
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+
+export const Image: React.FC<{ productImage: string[] }> = ({
+  productImage,
+}) => {
+  const renderImages = () => {
+    return productImage.map((image, index) => (
+      <SwiperSlide key={index} className="rounded-lg">
+        <div className="flex items-center justify-center">
+          <img src={image} className="w-[320px] h-[360px]" />;
+        </div>
+      </SwiperSlide>
+    ));
+  };
+
+  return (
+    <Swiper
+      slidesPerView={1}
+      pagination={true}
+      modules={[Pagination]}
+      style={
+        {
+          "--swiper-pagination-color": "#D9D9D9",
+          "--swiper-pagination-bullet-inactive-color": "#999999",
+          "--swiper-pagination-bullet-inactive-opacity": "1",
+        } as React.CSSProperties
+      }
+    >
+      {renderImages()}
+    </Swiper>
+  );
 };

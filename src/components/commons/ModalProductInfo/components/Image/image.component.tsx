1df6eb8d225ced585fdd50c7cb9f00d13a88176a
AugustoCVS <guty.vertolis@gmail.com>
2024-06-04 23:47:40 -0300

    feat: add width and height

--- src/components/commons/ModalProductInfo/components/Image/image.component.tsx
+++ src/components/commons/ModalProductInfo/components/Image/image.component.tsx
@@ -1,3 +1,3 @@
 export const Image: React.FC<{ src: string; alt: string }> = ({ src, alt }) => {
-  return <img src={src} alt={alt} className="bg-black w-full" />;
+  return <img src={src} alt={alt} className="bg-black w-[320px] h-[360px]" />;
 };
